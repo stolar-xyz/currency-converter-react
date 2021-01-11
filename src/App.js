9a47c88bc1c634c29863099f7deb7f0271143575
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-12 00:42:55 +0100

    NumberInput component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Paragraph from "./Paragraph";
 import FormField from "./FormField";
+import NumberInput from "./NumberInput"
 
 function App() {
   return (
@@ -8,24 +9,14 @@ function App() {
       <form className="form">
         <fieldset className="form__fieldset">
           <legend className="form__legend">Kalkulator walutowy</legend>
-          <p className="form__paragraph">
-            <label>
-
-              <span className="form__spanText">Kwota</span>
-
-              <input className="form__formField" step="any" min="0" placeholder="Wpisz kwotę"
-                type="number" />
-
-            </label>
-          </p>
+          <Paragraph name={"Kwota"} body={<NumberInput />} />
           <Paragraph name={"Posiadam"} body={<FormField />} />
           <Paragraph name={"Chcę otrzymać"} body={<FormField />} />
           <div className="form__buttonsContainer">
             <button className="form__button">Zmień</button>
             <button className="form__button">Przelicz</button>
           </div>
-          <p className="form__paragraph form__paragraph--special">
-          </p>
+          <Paragraph />
         </fieldset>
       </form>
     </div>
---
+++ src/NumberInput/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const NumberInput = () => (
+    <input className="form__formField js-input-form" step="any" min="0" placeholder="Wpisz kwotę"
+        type="number" />
+);
+
+export default NumberInput;
