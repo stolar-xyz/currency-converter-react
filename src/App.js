d6af2dd6083b74ae5e17a3e76bdab8863c5e3851
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-12 00:59:18 +0100

    Fieldset component added

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ import FormField from "./FormField";
 import NumberInput from "./NumberInput";
 import Buttons from "./Buttons";
 import Legend from "./Legend";
+import Fieldset from "./Fieldset";
 
 function App() {
   return (
-    <div className="container">
-      <form className="form">
-        <fieldset className="form__fieldset">
-          <Legend title={"Kalkulator walutowy"} />
-          <Paragraph name={"Kwota"} body={<NumberInput />} />
-          <Paragraph name={"Posiadam"} body={<FormField />} />
-          <Paragraph name={"Chcę otrzymać"} body={<FormField />} />
-          <Buttons />
-          <Paragraph />
-        </fieldset>
-      </form>
-    </div>
+    <form className="form">
+      <Fieldset>
+        <Legend title={"Kalkulator walutowy"} />
+        <Paragraph name={"Kwota"} body={<NumberInput />} />
+        <Paragraph name={"Posiadam"} body={<FormField />} />
+        <Paragraph name={"Chcę otrzymać"} body={<FormField />} />
+        <Buttons />
+        <Paragraph />
+      </Fieldset>
+    </form>
   );
 }
 
