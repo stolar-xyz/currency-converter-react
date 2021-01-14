e0a6fb00192c6baedf1ee0de2064a4c381426e41
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-15 00:21:37 +0100

    adding preventDefault to Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const Form = ({ children }) => (
-    <form className="form">
-        {children}
-    </form>
-);
+const Form = ({ children }) => {
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={onFormSubmit} className="form">
+            {children}
+        </form>
+    )
+};
 
 export default Form;
