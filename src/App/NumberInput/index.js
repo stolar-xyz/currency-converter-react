0d236d2cf874591ac0166226e881e6dc6c501f58
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-17 23:02:57 +0100

    required validation added to NumberInput component

--- src/App/NumberInput/index.js
+++ src/App/NumberInput/index.js
@@ -4,7 +4,7 @@ const NumberInput = ({ amount, setAmount }) => {
 
     return (
         <input value={amount} onChange={({ target }) => setAmount(target.value)} className="form__formField" step="any" min="0" placeholder="Enter the amount"
-            type="number" />
+            type="number" required />
     );
 };
 
