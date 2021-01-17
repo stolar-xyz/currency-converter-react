d4b4f1fb2609ba78f11be27f4041dc1885974296
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-17 20:57:06 +0100

    currencies.js file added and deleted options in FormField component

--- src/App/FormField/index.js
+++ src/App/FormField/index.js
@@ -2,14 +2,11 @@ import React, { useState } from "react";
 import "./style.css";
 
 const FormField = () => {
-    const [valute, setValute] = useState("PLN - Poland");
+    const [valute, setValute] = useState();
 
     return (
         <select className="form__formField" value={valute} onChange={({ target }) => setValute(target.value)}>
-            <option>PLN - Poland</option>
-            <option>EUR - European Union</option>
-            <option>USD - United States</option>
-            <option>GBP - United Kingdom</option>
+            <option></option>
         </select>
     );
 };
---
+++ src/App/currencies.js
@@ -0,0 +1,19 @@
+const currencies = [
+    {
+        short: "USD",
+        name: "U.S. Dollar",
+        rate: 3.76,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.54,
+    },
+    {
+        short: "GBP",
+        name: "British Pound",
+        rate: 5.11,
+    },
+];
+
+export default currencies;
