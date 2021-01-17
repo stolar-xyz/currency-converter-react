fbc8c9ce1249b1354ac1666c667e6bc3adbb9e96
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-17 22:15:08 +0100

    added depenting options form currencies.js file

--- src/App/FormField/index.js
+++ src/App/FormField/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
+import currencies from "../currencies";
 
 const FormField = ({ currency, setCurrency }) => {
 
     return (
         <select className="form__formField" value={currency} onChange={({ target }) => setCurrency(target.value)}>
-            <option></option>
+            {currencies.map((currency => (
+                <option
+                    key={currency.short}
+                    value={currency.short}
+                >
+                    {currency.name}
+                </option>
+            )))}
         </select>
     );
 };
