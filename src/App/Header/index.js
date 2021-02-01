b3aec6c58db4d18476ddebedb565bce7060b5b6a
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-01 15:04:10 +0100

    added header tag to Header

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { StyledHeader } from "./styled";
 
 const Header = ({ title }) => (
-    <StyledHeader>
-        {title}
-    </StyledHeader>
+    <header>
+        <StyledHeader>
+            {title}
+        </StyledHeader>
+    </header>
 );
 
 export default Header;
