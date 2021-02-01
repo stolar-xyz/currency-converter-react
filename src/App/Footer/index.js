0feaf555aa8974ae9b81d70a5b4496356cf81d0c
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-01 14:48:12 +0100

    styled component added to Footer

--- src/App/Footer/index.js
+++ src/App/Footer/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./style.css";
+import { StyledFooter } from "./styled";
 
 const Footer = () => (
-    <footer className="form__footer">
+    <StyledFooter>
         The rates are taken from the nbp.pl website from Table 010/A/NBP/2021 of 2021-01-17
-    </footer>
+    </StyledFooter>
 );
 
 export default Footer;
---
+++ src/App/Footer/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+    margin: 30px;
+
+    @media (max-width: 767px) {
+        font-size: 14px;
+    }
+`;
