42ef65d763d12ead56d4376df6b161d6a5b01d83
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-07 18:30:18 +0200

    corrected API_KEY

--- src/App/useRatesData.js
+++ src/App/useRatesData.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_KEY = "6977e0053d1b260e9d5bc3a76953732f/";
+const API_KEY = "6977e0053d1b260e9d5bc3a76953732f";
 const currencies = ["USD","PLN","JPY","GBP","AUD","CAD","CHF","CNH","SEK","NZD"];
 
 const exchangeRatesAPI = `http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}&symbols=${currencies}`;
